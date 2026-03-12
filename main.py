5446c45b4d8d5d98d2122af02107e0e3c6ccfeef
Juanesco100 <juanse01sebas@gmail.com>
2026-03-12 06:55:12 -0500

    Final version

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from script import calculate
-calculate()
+from script import register_sales, calculate_total, show_summary
+
+sales = []
+product_sales = []
+
+data = register_sales(sales, product_sales)
+
+product_sales = data[0]
+sales = data[1]
+buyer = data[2]
+
+total = calculate_total(sales)
+
+show_summary(product_sales, sales, buyer, total)
